e99033c68446c3d34ae17684270e8990b7e8d8a2
Aleksander Philips <aleksander.philips@gmail.com>
2015-03-20 15:03:03 +0100

    UISynchronizer poprawka

--- CinemaProject/Common/UISynchronizer.cs
+++ CinemaProject/Common/UISynchronizer.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Common
 {
-    class UISynchronizer
+    public class UISynchronizer
     {
+        public static void synchronizeWithUI<TParam>(Control control, Action<TParam> action, TParam param)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action, param);
+            }
+            else
+            {
+                action(param);
+            }
+        }
+
+        public static void synchronizeWithUI(Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        public static void enableButton(Button b, Boolean enable)
+        {
+            UISynchronizer.synchronizeWithUI(b, (boolean) => b.Enabled = boolean, enable);
+        }
+
     }
 }
