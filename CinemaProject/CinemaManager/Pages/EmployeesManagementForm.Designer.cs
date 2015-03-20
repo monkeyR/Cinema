df140fafced75c874f889d4713989f425cab465e
Aleksander Philips <aleksander.philips@gmail.com>
2015-03-20 16:14:15 +0100

    widok modułu pracowników

--- CinemaProject/CinemaManager/Pages/EmployeesManagementForm.Designer.cs
+++ CinemaProject/CinemaManager/Pages/EmployeesManagementForm.Designer.cs
@@ -28,33 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.EmployeesButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.employeeListView = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.addNewEmployeeButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.firedListView = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // EmployeesButton
+            // panel2
             // 
-            this.EmployeesButton.Location = new System.Drawing.Point(259, 236);
-            this.EmployeesButton.Margin = new System.Windows.Forms.Padding(2);
-            this.EmployeesButton.Name = "EmployeesButton";
-            this.EmployeesButton.Size = new System.Drawing.Size(202, 44);
-            this.EmployeesButton.TabIndex = 3;
-            this.EmployeesButton.Text = "Pracownicy";
-            this.EmployeesButton.UseVisualStyleBackColor = true;
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Controls.Add(this.addNewEmployeeButton);
+            this.panel2.Controls.Add(this.employeeListView);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(696, 263);
+            this.panel2.TabIndex = 17;
+            // 
+            // employeeListView
+            // 
+            this.employeeListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.employeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.employeeListView.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.employeeListView.FullRowSelect = true;
+            this.employeeListView.GridLines = true;
+            this.employeeListView.Location = new System.Drawing.Point(3, 51);
+            this.employeeListView.MultiSelect = false;
+            this.employeeListView.Name = "employeeListView";
+            this.employeeListView.Size = new System.Drawing.Size(690, 207);
+            this.employeeListView.TabIndex = 6;
+            this.employeeListView.UseCompatibleStateImageBehavior = false;
+            this.employeeListView.View = System.Windows.Forms.View.Details;
+            this.employeeListView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.employeeListView_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lista pracowników";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Imię";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nazwisko";
+            this.columnHeader2.Width = 212;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Stanowisko";
+            this.columnHeader3.Width = 173;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Login";
+            this.columnHeader4.Width = 140;
+            // 
+            // addNewEmployeeButton
+            // 
+            this.addNewEmployeeButton.Location = new System.Drawing.Point(603, 2);
+            this.addNewEmployeeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.addNewEmployeeButton.Name = "addNewEmployeeButton";
+            this.addNewEmployeeButton.Size = new System.Drawing.Size(90, 44);
+            this.addNewEmployeeButton.TabIndex = 18;
+            this.addNewEmployeeButton.Text = "Dodaj nowego";
+            this.addNewEmployeeButton.UseVisualStyleBackColor = true;
+            this.addNewEmployeeButton.Click += new System.EventHandler(this.addNewEmployeeButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.firedListView);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 281);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 223);
+            this.panel1.TabIndex = 19;
+            // 
+            // firedListView
+            // 
+            this.firedListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.firedListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.firedListView.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.firedListView.FullRowSelect = true;
+            this.firedListView.GridLines = true;
+            this.firedListView.Location = new System.Drawing.Point(3, 51);
+            this.firedListView.MultiSelect = false;
+            this.firedListView.Name = "firedListView";
+            this.firedListView.Size = new System.Drawing.Size(690, 169);
+            this.firedListView.TabIndex = 6;
+            this.firedListView.UseCompatibleStateImageBehavior = false;
+            this.firedListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Imię";
+            this.columnHeader5.Width = 160;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Nazwisko";
+            this.columnHeader6.Width = 212;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Stanowisko";
+            this.columnHeader7.Width = 173;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Login";
+            this.columnHeader8.Width = 140;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lista zwolnionych";
             // 
             // EmployeesManagementForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(720, 516);
-            this.Controls.Add(this.EmployeesButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "EmployeesManagementForm";
             this.Text = "EmployeesManagementForm";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button EmployeesButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView employeeListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addNewEmployeeButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView firedListView;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Label label1;
+
     }
 }
--- CinemaProject/CinemaManager/Pages/EmployeesManagementForm.cs
+++ CinemaProject/CinemaManager/Pages/EmployeesManagementForm.cs
@@ -15,6 +15,97 @@ namespace CinemaManager.Pages
         public EmployeesManagementForm()
         {
             InitializeComponent();
+            
+            startFilling();
+        }
+
+        private void startFilling()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += fillWithData;
+            worker.RunWorkerCompleted += onWorkerCompleted;
+            Cursor.Current = Cursors.WaitCursor;
+            worker.RunWorkerAsync();
+        }
+
+        private void onWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+        }
+        private void fillWithData(object sender, DoWorkEventArgs e)
+        {
+            Common.UISynchronizer.synchronizeWithUI(employeeListView, employeeListView.Items.Clear);
+
+            initializeEmployeeListView();
+        }
+
+        private void initializeEmployeeListView()
+        {
+            using (CinemaModel.CinemaDatabaseEntities ctx = new CinemaModel.CinemaDatabaseEntities())
+            {
+                var employees = ctx.Employees;
+
+                foreach (var employee in employees)
+                {
+                    List<string> listItem = new List<string>(4);
+
+                    listItem.Add(employee.name);
+                    listItem.Add(employee.surname);
+                    listItem.Add(employee.login);
+                    switch (employee.position)
+                    {
+                        case (int)Common.EmployeeTypes.Types.Manager:
+                            listItem.Add("Manager");
+                            break;
+                        case (int)Common.EmployeeTypes.Types.Saler:
+                            listItem.Add("Sprzedawca");
+                            break;
+                    }
+
+                    ListViewItem item = new ListViewItem(listItem.ToArray());
+
+                    Common.UISynchronizer.synchronizeWithUI(employeeListView, x => employeeListView.Items.Add(x), item);
+                }
+            }
+        }
+
+        private void addNewEmployeeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeeListView_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right && employeeListView.FocusedItem != null)
+            {
+                ContextMenu cm = new ContextMenu();
+
+                MenuItem editEmployeeItem = new MenuItem("Edytuj pracownika");
+                editEmployeeItem.Click += (objectSender, args) =>
+                    {
+                        editEmployeeItemClick(objectSender, args);
+                    };
+                cm.MenuItems.Add(editEmployeeItem);
+
+                MenuItem fireEmployeeItem = new MenuItem("Zwolnij pracownika");
+                fireEmployeeItem.Click += (objectSender, args) =>
+                    {
+                        fireEmployeeItemClick(objectSender, args);
+                    };
+                cm.MenuItems.Add(fireEmployeeItem);
+
+                cm.Show(this, this.PointToClient(Cursor.Position));
+            }
+        }
+
+        private void fireEmployeeItemClick(object objectSender, EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void editEmployeeItemClick(object objectSender, EventArgs args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
