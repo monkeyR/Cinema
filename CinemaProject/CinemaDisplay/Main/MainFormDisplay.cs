cf625a5b8b0933daa7db96ad744a1b1ef637a8ba
kejmil01 <kejmil01@gmail.com>
2015-04-13 23:57:58 +0200

    (Repertuar)Pierwsze kroki.

---
+++ CinemaProject/CinemaDisplay/Main/MainFormDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaDisplay.Main
+{
+    public partial class MainFormDisplay : Form
+    {
+        public MainFormDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFormDisplay_KeyUp(object sender, KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.KeyCode == Keys.Escape)
+                askForLeaving();
+        }
+
+        private void askForLeaving()
+        {
+            DialogResult dialogResult = MessageBox.Show("Sure", "Some Title", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
