dd8856c582eb0e77830bbd4e27ded8c0adad9789
monkeyR <monika.raszewska.mr@gmail.com>
2015-04-28 11:57:26 +0200

    Commit - Add new hall

---
+++ CinemaProject/CinemaManager/SubPages/AddNewHall.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace CinemaManager.SubPages
+{
+    partial class AddNewHall
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HallAddNewPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HallCreateTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.HallCreatePanel = new System.Windows.Forms.Panel();
+            this.ScreenLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.HallInfo = new System.Windows.Forms.Panel();
+            this.ColumnsLabel = new System.Windows.Forms.Label();
+            this.RowsLabel = new System.Windows.Forms.Label();
+            this.HallNameLabel = new System.Windows.Forms.Label();
+            this.HallCreatorHallNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CreateHallONButton = new System.Windows.Forms.Button();
+            this.HallCreatorColumsNumberTextBox = new System.Windows.Forms.TextBox();
+            this.HallCreateAddHallButton = new System.Windows.Forms.Button();
+            this.HallCreatorRowsNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HallAddNewPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.HallCreatePanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.HallInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HallAddNewPanel
+            // 
+            this.HallAddNewPanel.AutoSize = true;
+            this.HallAddNewPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(156)))), ((int)(((byte)(204)))));
+            this.HallAddNewPanel.Controls.Add(this.panel1);
+            this.HallAddNewPanel.Controls.Add(this.HallCreatePanel);
+            this.HallAddNewPanel.Controls.Add(this.groupBox1);
+            this.HallAddNewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HallAddNewPanel.Location = new System.Drawing.Point(0, 0);
+            this.HallAddNewPanel.Name = "HallAddNewPanel";
+            this.HallAddNewPanel.Size = new System.Drawing.Size(1103, 580);
+            this.HallAddNewPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.HallCreateTableLayoutPanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 171);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1103, 409);
+            this.panel1.TabIndex = 12;
+            // 
+            // HallCreateTableLayoutPanel
+            // 
+            this.HallCreateTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HallCreateTableLayoutPanel.AutoScroll = true;
+            this.HallCreateTableLayoutPanel.AutoSize = true;
+            this.HallCreateTableLayoutPanel.ColumnCount = 1;
+            this.HallCreateTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.HallCreateTableLayoutPanel.Location = new System.Drawing.Point(104, 27);
+            this.HallCreateTableLayoutPanel.Name = "HallCreateTableLayoutPanel";
+            this.HallCreateTableLayoutPanel.RowCount = 1;
+            this.HallCreateTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.HallCreateTableLayoutPanel.Size = new System.Drawing.Size(895, 348);
+            this.HallCreateTableLayoutPanel.TabIndex = 16;
+            // 
+            // HallCreatePanel
+            // 
+            this.HallCreatePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(156)))), ((int)(((byte)(204)))));
+            this.HallCreatePanel.Controls.Add(this.ScreenLabel);
+            this.HallCreatePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HallCreatePanel.Location = new System.Drawing.Point(0, 128);
+            this.HallCreatePanel.Name = "HallCreatePanel";
+            this.HallCreatePanel.Size = new System.Drawing.Size(1103, 43);
+            this.HallCreatePanel.TabIndex = 11;
+            // 
+            // ScreenLabel
+            // 
+            this.ScreenLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ScreenLabel.AutoSize = true;
+            this.ScreenLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScreenLabel.Location = new System.Drawing.Point(0, 0);
+            this.ScreenLabel.Name = "ScreenLabel";
+            this.ScreenLabel.Size = new System.Drawing.Size(81, 23);
+            this.ScreenLabel.TabIndex = 0;
+            this.ScreenLabel.Text = "EKRAN";
+            this.ScreenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(156)))), ((int)(((byte)(204)))));
+            this.groupBox1.Controls.Add(this.HallInfo);
+            this.groupBox1.Controls.Add(this.HallCreateAddHallButton);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1103, 128);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // HallInfo
+            // 
+            this.HallInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HallInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(156)))), ((int)(((byte)(204)))));
+            this.HallInfo.Controls.Add(this.ColumnsLabel);
+            this.HallInfo.Controls.Add(this.HallCreatorHallNameTextBox);
+            this.HallInfo.Controls.Add(this.RowsLabel);
+            this.HallInfo.Controls.Add(this.label3);
+            this.HallInfo.Controls.Add(this.HallNameLabel);
+            this.HallInfo.Controls.Add(this.CreateHallONButton);
+            this.HallInfo.Controls.Add(this.HallCreatorColumsNumberTextBox);
+            this.HallInfo.Controls.Add(this.label2);
+            this.HallInfo.Controls.Add(this.label1);
+            this.HallInfo.Controls.Add(this.HallCreatorRowsNumberTextBox);
+            this.HallInfo.Location = new System.Drawing.Point(6, 18);
+            this.HallInfo.Name = "HallInfo";
+            this.HallInfo.Size = new System.Drawing.Size(993, 107);
+            this.HallInfo.TabIndex = 7;
+            // 
+            // ColumnsLabel
+            // 
+            this.ColumnsLabel.AutoSize = true;
+            this.ColumnsLabel.Location = new System.Drawing.Point(326, 58);
+            this.ColumnsLabel.Name = "ColumnsLabel";
+            this.ColumnsLabel.Size = new System.Drawing.Size(66, 17);
+            this.ColumnsLabel.TabIndex = 2;
+            this.ColumnsLabel.Text = "Kolumny:";
+            // 
+            // RowsLabel
+            // 
+            this.RowsLabel.AutoSize = true;
+            this.RowsLabel.Location = new System.Drawing.Point(326, 37);
+            this.RowsLabel.Name = "RowsLabel";
+            this.RowsLabel.Size = new System.Drawing.Size(56, 17);
+            this.RowsLabel.TabIndex = 1;
+            this.RowsLabel.Text = "Rzędy: ";
+            // 
+            // HallNameLabel
+            // 
+            this.HallNameLabel.AutoSize = true;
+            this.HallNameLabel.Location = new System.Drawing.Point(326, 16);
+            this.HallNameLabel.Name = "HallNameLabel";
+            this.HallNameLabel.Size = new System.Drawing.Size(83, 17);
+            this.HallNameLabel.TabIndex = 0;
+            this.HallNameLabel.Text = "Nazwa sali: ";
+            // 
+            // HallCreatorHallNameTextBox
+            // 
+            this.HallCreatorHallNameTextBox.Location = new System.Drawing.Point(111, 1);
+            this.HallCreatorHallNameTextBox.Name = "HallCreatorHallNameTextBox";
+            this.HallCreatorHallNameTextBox.Size = new System.Drawing.Size(97, 22);
+            this.HallCreatorHallNameTextBox.TabIndex = 6;
+            this.HallCreatorHallNameTextBox.Text = "Siema";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nazwa sali:";
+            // 
+            // CreateHallONButton
+            // 
+            this.CreateHallONButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(62)))), ((int)(((byte)(68)))));
+            this.CreateHallONButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CreateHallONButton.ForeColor = System.Drawing.Color.White;
+            this.CreateHallONButton.Location = new System.Drawing.Point(215, 0);
+            this.CreateHallONButton.Margin = new System.Windows.Forms.Padding(4);
+            this.CreateHallONButton.Name = "CreateHallONButton";
+            this.CreateHallONButton.Size = new System.Drawing.Size(65, 103);
+            this.CreateHallONButton.TabIndex = 0;
+            this.CreateHallONButton.Text = "Dalej";
+            this.CreateHallONButton.UseVisualStyleBackColor = false;
+            this.CreateHallONButton.Click += new System.EventHandler(this.CreateHallONButton_Click_2);
+            // 
+            // HallCreatorColumsNumberTextBox
+            // 
+            this.HallCreatorColumsNumberTextBox.Location = new System.Drawing.Point(111, 41);
+            this.HallCreatorColumsNumberTextBox.Name = "HallCreatorColumsNumberTextBox";
+            this.HallCreatorColumsNumberTextBox.Size = new System.Drawing.Size(97, 22);
+            this.HallCreatorColumsNumberTextBox.TabIndex = 4;
+            this.HallCreatorColumsNumberTextBox.Text = "16";
+            // 
+            // HallCreateAddHallButton
+            // 
+            this.HallCreateAddHallButton.AutoSize = true;
+            this.HallCreateAddHallButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(62)))), ((int)(((byte)(68)))));
+            this.HallCreateAddHallButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.HallCreateAddHallButton.ForeColor = System.Drawing.Color.White;
+            this.HallCreateAddHallButton.Image = global::CinemaManager.Properties.Resources.symbol_check_icon;
+            this.HallCreateAddHallButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.HallCreateAddHallButton.Location = new System.Drawing.Point(1014, 18);
+            this.HallCreateAddHallButton.Name = "HallCreateAddHallButton";
+            this.HallCreateAddHallButton.Size = new System.Drawing.Size(86, 107);
+            this.HallCreateAddHallButton.TabIndex = 1;
+            this.HallCreateAddHallButton.Text = "Zatwierdź";
+            this.HallCreateAddHallButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.HallCreateAddHallButton.UseVisualStyleBackColor = false;
+            this.HallCreateAddHallButton.Click += new System.EventHandler(this.HallCreateAddHallButton_Click_1);
+            // 
+            // HallCreatorRowsNumberTextBox
+            // 
+            this.HallCreatorRowsNumberTextBox.Location = new System.Drawing.Point(111, 81);
+            this.HallCreatorRowsNumberTextBox.Name = "HallCreatorRowsNumberTextBox";
+            this.HallCreatorRowsNumberTextBox.Size = new System.Drawing.Size(97, 22);
+            this.HallCreatorRowsNumberTextBox.TabIndex = 3;
+            this.HallCreatorRowsNumberTextBox.Text = "12";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ilość rzędów:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ilość kolumn:";
+            // 
+            // AddNewHall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.HallAddNewPanel);
+            this.Name = "AddNewHall";
+            this.Size = new System.Drawing.Size(1103, 580);
+            this.HallAddNewPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.HallCreatePanel.ResumeLayout(false);
+            this.HallCreatePanel.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.HallInfo.ResumeLayout(false);
+            this.HallInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel HallAddNewPanel;
+        private System.Windows.Forms.Panel HallCreatePanel;
+        private System.Windows.Forms.Label ScreenLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel HallInfo;
+        private System.Windows.Forms.Label ColumnsLabel;
+        private System.Windows.Forms.Label RowsLabel;
+        private System.Windows.Forms.Label HallNameLabel;
+        private System.Windows.Forms.TextBox HallCreatorHallNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CreateHallONButton;
+        private System.Windows.Forms.TextBox HallCreatorColumsNumberTextBox;
+        private System.Windows.Forms.Button HallCreateAddHallButton;
+        private System.Windows.Forms.TextBox HallCreatorRowsNumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel HallCreateTableLayoutPanel;
+
+    }
+}
