9e21696919218ea27605791fe15306336aca7553
Kamil <kejmil01@gmail.com>
2015-05-11 13:45:20 +0200

    Dostepnosc filmu, Model entity
    Uwzglednienie informacji o dostepnosci filmu przy dodawaniu seansów.
    Update modelu Entity(isAvaible not null)

--- CinemaProject/CinemaModel/CinemaEntities.Context.cs
+++ CinemaProject/CinemaModel/CinemaEntities.Context.cs
@@ -30,7 +30,6 @@ namespace CinemaModel
         public virtual DbSet<Categories> Categories { get; set; }
         public virtual DbSet<Employees> Employees { get; set; }
         public virtual DbSet<Halls> Halls { get; set; }
-        public virtual DbSet<Movies> Movies { get; set; }
         public virtual DbSet<MovieSales> MovieSales { get; set; }
         public virtual DbSet<Products> Products { get; set; }
         public virtual DbSet<ProductSales> ProductSales { get; set; }
@@ -38,6 +37,7 @@ namespace CinemaModel
         public virtual DbSet<Shows> Shows { get; set; }
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Workpositions> Workpositions { get; set; }
+        public virtual DbSet<Movies> Movies { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
